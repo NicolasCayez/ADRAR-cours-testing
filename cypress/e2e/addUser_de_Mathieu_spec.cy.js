62be00fa9bb97a4b733f1dd2fbc3595e631ecb14
NicolasCayez <nicolascayez@gmail.com>
2024-10-31 14:03:08 +0100

    exo pokeliste correction

--- cypress/e2e/addUser_de_Mathieu_spec.cy.js
+++ cypress/e2e/addUser_de_Mathieu_spec.cy.js
@@ -1,28 +1,47 @@
-describe(`Counter spec`, () => {
+describe(`addUser spec`, () => {
 
-  it(`Chargement Adding Numbers`, () => {
+  it(`Chargement addUser`, () => {
     cy.visit(`https://testing.adrardev.fr/addUser`)
-  })
-  
+  });
   beforeEach(() => {
     cy.visit("https://testing.adrardev.fr/addUser");
     // Visite de l'URL avant chaque test
   });
 
-  // it(`La div PokeListe doit exister`, () => {
-  //   cy.get("#pokeListe")
-  // })
+  it(`Tous les inputs du formulaire sont présents et ont les bons attributs name`, () => {
+    cy.get("form").children('input').eq(0).should('have.attr', 'name', 'nom');
+    cy.get("form").children('input').eq(1).should('have.attr', 'name', 'prenom');
+    cy.get("form").children('input').eq(2).should('have.attr', 'name', 'mail');
+    cy.get("form").children('input').eq(3).should('have.attr', 'name', 'mdp');
+  });
 
-  // it(`La réponse de l'api est 200`, () => {
-  //   cy.request('GET', 'https://pokeapi.co/api/v2/pokemon').then((response) =>{
-  //     //Expecting the response status code to be 200
-  //     expect(response.status).to.eq(200)})
-  // })
+  it(`Le formulaire vérifie si tous les champs ont été remmplis (4 tests)`, () => {
+    // cy.get("form").children('input').eq(0).type('Riri');
+    cy.get("form").children('input').eq(1).type('Fifi');
+    cy.get("form").children('input').eq(2).type('loulou@labandeapicsou.com');
+    cy.get("form").children('input').eq(3).type('pathibulaire');
+    cy.get("form").children('input').eq(4).click();
+    cy.get("#msgzone").should('have.text', 'Veuillez remplir tous les champs du formulaire');
 
-  // it(`Devrait mesurer le temps de réponse de l'api (<2000ms)`, () => {
-  //   cy.request('GET', 'https://pokeapi.co/api/v2/pokemon').then((response) =>{
-  //     //Expecting the response status code to be 200
-  //     console.log(response)
-  //     expect(response.duration).to.lessThan(2000)})
-  // })
-})
+    cy.get("form").children('input').eq(0).type('Riri');
+    // cy.get("form").children('input').eq(1).type('Fifi');
+    cy.get("form").children('input').eq(2).type('loulou@labandeapicsou.com');
+    cy.get("form").children('input').eq(3).type('pathibulaire');
+    cy.get("form").children('input').eq(4).click();
+    cy.get("#msgzone").should('have.text', 'Veuillez remplir tous les champs du formulaire');
+
+    cy.get("form").children('input').eq(0).type('Riri');
+    cy.get("form").children('input').eq(1).type('Fifi');
+    // cy.get("form").children('input').eq(2).type('loulou@labandeapicsou.com');
+    cy.get("form").children('input').eq(3).type('pathibulaire');
+    cy.get("form").children('input').eq(4).click();
+    cy.get("#msgzone").should('have.text', 'Veuillez remplir tous les champs du formulaire');
+
+    cy.get("form").children('input').eq(0).type('Riri');
+    cy.get("form").children('input').eq(1).type('Fifi');
+    cy.get("form").children('input').eq(2).type('loulou@labandeapicsou.com');
+    // cy.get("form").children('input').eq(3).type('pathibulaire');
+    cy.get("form").children('input').eq(4).click();
+    cy.get("#msgzone").should('have.text', 'Veuillez remplir tous les champs du formulaire');
+  });
+});
--- cypress/e2e/pokelist_spec.cy.js
+++ cypress/e2e/pokelist_spec.cy.js
@@ -2,7 +2,7 @@ describe(`PokeListe spec`, () => {
 
   it(`Chargement Pokelist`, () => {
     cy.visit(`localhost:5173/pokelist.html`)
-  })
+  });
   
   beforeEach(() => {
     cy.visit("http://localhost:5173/pokelist.html");
@@ -10,38 +10,43 @@ describe(`PokeListe spec`, () => {
   });
 
   it(`La div PokeListe doit exister`, () => {
-    cy.get("#pokeListe")
-  })
+    cy.get("#pokeListe");
+  });
 
   it(`La réponse de l'api est 200`, () => {
-    cy.request('GET', 'https://pokeapi.co/api/v2/pokemon').then((response) =>{
-      //Expecting the response status code to be 200
-      expect(response.status).to.eq(200)
-    })
-  })
+    // cy.request('GET', 'https://pokeapi.co/api/v2/pokemon').then((response) =>{
+    //   //Expecting the response status code to be 200
+    //   expect(response.status).to.eq(200)
+    // })
+    cy.request('GET', 'https://pokeapi.co/api/v2/pokemon')
+    .its('status').should('equal', 200);
+  });
 
-  it(`Devrait mesurer le temps de réponse de l'api (<2000ms)`, () => {
-    cy.request('GET', 'https://pokeapi.co/api/v2/pokemon').then((response) =>{
-      //Expecting the response status code to be 200
-      console.log(response)
-      expect(response.duration).to.lessThan(2000)
-    })
-  })
+  it('Devrait mesurer le temps de réponse de l API (inf à 2000ms)', () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon';
+    const startTime = performance.now(); // Démarre le chrono
+    cy.request('GET', url).then((response) => {
+        const endTime = performance.now(); // Arrête le chronom
+        const duration = endTime - startTime; // Calcule la durée
+        expect(response.status).to.eq(200); // Vérifie que le statut est 200
+        expect(duration).to.be.lessThan(2000); // Vérifie que le temps de réponse est inférieur à 2000 ms
+    });
+  });
   
   it(`Devrait vérifier que bulbasaur est bien le premier affiché`, () => {
-    cy.get("#pokeListe").children().eq(0).should("have.text", 'bulbasaur')
-  })
+    cy.get("#pokeListe").children().eq(0).should("have.text", 'bulbasaur');
+  });
   
   it(`Devrait vérifier que la réponse a bien les champs name et url`, () => {
     cy.request('GET', 'https://pokeapi.co/api/v2/pokemon').then((response) =>{
-      console.log(response)
-      expect(response.name, response.url)
+      console.log(response);
+      // expect(response.name, response.url);
+      expect(response.body).to.have.property('results');// vérif présence results
+      expect(response.body.results[0]).to.have.all.keys('name', 'url');//v&rif des clés du premier pokemon
     })
-  })
+  });
 
   it(`Devrait afficher un message d'erreur si l'api échoue`, () => {
-    // cy.request('GET', 'https://pokeapi.co/api/v2/pokemon2').then((response) =>{
-    //   console.log(response)
-    // })
+    cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon', { statusCode: 500 }).as('getPokemonError');
   })
-})
+});
